ea68078903739abe2bc57d04062193b76b8e3388
Andrey Aksarin <aandrew773@gmail.com>
2025-02-17 02:03:44 +0500

    1th commit (create models of buyers)

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'apps.profiles',
     'apps.sellers',
     'apps.shop',
+    'apps.common',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "accounts.User"
