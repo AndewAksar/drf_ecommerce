ca4b34a595192df7d1f4b5cdd82f904c926a94e6
Andrey Aksarin <aandrew773@gmail.com>
2025-02-20 01:41:45 +0500

    5th commit (install DRF Spectacular)

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'apps.accounts',
     'apps.profiles',
     'apps.sellers',
@@ -127,4 +128,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django REST Framework API',
+    'DESCRIPTION': 'A sample API for Django REST Framework by Andrew Aksarin',
+    'VERSION': '1.0.0',
+}
